7a77c8e854cddc5dc6a61333010b1cc812b4ee76
Niall Carroll <niallpcarroll@gmail.com>
2024-11-27 18:15:06 +0000

    Update blog post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.views import generic
+from .models import Post
 
-def my_blog(request):
-    return HttpResponse("Hello, Musical Musings!")
+# Create your views here.
+class PostList(generic.ListView):
+    model = Post
