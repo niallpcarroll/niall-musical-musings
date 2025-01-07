9904d4c9892ecd6967859ed3b3d22245a225e395
Niall Carroll <niallpcarroll@gmail.com>
2025-01-07 17:42:45 +0000

    Re-install summernote

--- musical_musings/settings.py
+++ musical_musings/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '*2f21vx2@13_pu0*ossff+w_6ohuae87#ecpve^7vtuaqe+thj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-niallpcarro-niallmusica-4m76ilk3ah8.ws.codeinstitute-ide.net",
 ".herokuapp.com"
